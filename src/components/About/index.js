d430c2f59accc6e8512d1e3654bd2a43a397ea11
Lydia Loy <lydia_art@yahoo.com>
2022-02-02 17:40:47 -0600

    added assets

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import About from './components/About';
+import coverImage from "../../assets/cover/cover-image.jpg"
+
+function App() {
+
+  return (
+    <div>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
